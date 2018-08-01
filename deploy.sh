b4aba37933fae7bc8d233f240310ef3f7ad5c4b8
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2018-08-02 00:50:27 +0700

    ssl-allow no

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-LOCALPATH='./.vuepress/dist'
+LOCALPATH='./docs/.vuepress/dist'
 REMOTEPATH='/docs'
 
 lftp -f "
+set dns:order "inet"
+set ftp:ssl-allow no
 open ftp://$FTP_HOST
 user $FTP_USER $FTP_PASSWORD
 mirror --continue --reverse --delete $LOCALPATH $REMOTEPATH
