51ecb0f68bd44935f59139e6bb7d2db8b9d3b39b
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2018-08-02 12:26:37 +0700

    home page

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: 'BAK Docs',
+  description: 'Dokumentasi Aplikasi BAK',
+  head: [
+    ['link', { rel: 'icon', href: 'favicon.ico' }]
+  ],
+  themeConfig: {
+    logo: 'favicon.ico',
+  },
+}
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,8 @@
-# Halo Bak
+---
+home: true
+heroImage: /logo.jpg
+actionText: Mulai →
+actionLink: /umum/
+footer: Universitas Yudharta Pasuruan | Copyright © 2018
+---
+
