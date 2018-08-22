bc841c26540f2de2381870bc449a4dd4fa26adf7
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2018-08-23 04:24:50 +0700

    algolia apiKey

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,6 +6,10 @@ module.exports = {
   ],
   themeConfig: {
     logo: '/favicon.ico',
+    algolia: {
+      apiKey: '8e2bdb0e169fd9d3e4b9d88ff8cea7df',
+      indexName: 'bak'
+    },
     nav: [
       { text: 'Tutorial', link: '/tutorial/' },
       { text: 'Website BAK', link: 'https://bak.yudharta.ac.id' },
